23e3a7bfe3377e3fce4bca080508bf2678b877fa
pedro <progerios@hotmail.com>
2025-02-15 12:30:44 -0300

    feat: add Produto model class

--- WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
+++ WebApplication/obj/Debug/net8.0/WebApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2881175ad9e3a097ef8b7fe968f14c861dda2a19")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
